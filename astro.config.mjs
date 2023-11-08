c8d3631d934c1bee14c2a8ab879ee3b8fa3e1f0f
ff6347 <hash@ff6347.email>
2023-11-08 18:00:07 +0100

    chore: Configure netlify build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/functions";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+	integrations: [react()],
+	output: "server",
+	adapter: netlify(),
 });
