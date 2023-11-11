d74c99bd53f41369607353f41f2c56ea7cdde944
ff6347 <hash@ff6347.email>
2023-11-11 09:12:14 +0100

    ci: Use SSR on netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
 import react from "@astrojs/react";
-
+const isOnNetlifyCI = process.env.NETLIFY === "true";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
-	// output: "server",
-	// adapter: netlify(),
+	output: isOnNetlifyCI ? "server" : undefined,
+	adapter: isOnNetlifyCI ? netlify() : undefined,
 });
