fcefd0ecbd95fc68af436a49ce78af029041d54e
ff6347 <hash@ff6347.email>
2023-11-11 10:03:26 +0100

    test(playwright): Set test dir to e2e
    move screenshots to subfolder of e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ const ciConfig = {
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-	testDir: "./tests",
+	testDir: "./e2e",
 	/* Run tests in files in parallel */
 	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
