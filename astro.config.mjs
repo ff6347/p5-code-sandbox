65aef21b2506cb2f9cc593d4b3f59054f23067a4
ff6347 <hash@ff6347.email>
2023-11-11 09:17:01 +0100

    refactor(netlify): No need for ssr
    The docs say:
    Consider enabling on-demand server rendering in your Astro project if you need the following:
    
    API endpoints: Create specific pages that function as API endpoints for tasks like database access, authentication, and authorization while keeping sensitive data hidden from the client.
    
    Protected pages: Restrict access to a page based on user privileges, by handling user access on the server.
    
    Frequently changing content: Generate individual pages without requiring a static rebuild of your site. This is useful when the content of a page updates frequently.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify/functions";
 import react from "@astrojs/react";
-const isOnNetlifyCI = process.env.NETLIFY === "true";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
-	output: isOnNetlifyCI ? "server" : undefined,
-	adapter: isOnNetlifyCI ? netlify() : undefined,
 });
