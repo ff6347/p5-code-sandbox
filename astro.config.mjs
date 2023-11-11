09b50b6b86065ed30da6a9322c704ea1b447c17d
ff6347 <hash@ff6347.email>
2023-11-11 08:58:19 +0100

    test(playwright): Add basic config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
-	output: "server",
-	adapter: netlify(),
+	// output: "server",
+	// adapter: netlify(),
 });
